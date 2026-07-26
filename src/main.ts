a479be867a8907f786b2137a7adbe73e3ebd1aaf
juuxi <dimashopyrev@mail.ru>
2026-07-26 11:34:25 +0300

    delete vite counter

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 import './style.css'
-import { setupCounter } from './counter.ts'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
 <h3>Question 1</h3>
@@ -7,8 +6,4 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
 <button>Option 2</button>
 <button>Option 3</button>
 <button>Option 4</button>
-
-<button id='counter' type='button' class='counter'></button>
 `
-
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
