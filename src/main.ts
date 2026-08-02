65e204fc444ee2410635d71c7a51d9547ccd2b59
juuxi <dimashopyrev@mail.ru>
2026-08-02 20:19:08 +0300

    separate main page and question htmls

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,4 @@
 import './style.css'
 import { setupQuestionServer } from './question_server.ts';
 
-
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-<div class='question_div'>
-  <h3>Question 1</h3>
-  <button class='counter'>Option 1</button>
-  <button class='counter'>Option 2</button>
-  <button class='counter'>Option 3</button>
-  <button class='counter'>Option 4</button>
-</div>
-
-<button id='next_btn'>Next</button>
-<button id='prev_btn'>Prev</button>
-
-<div class='debug'></div>
-`
-
 setupQuestionServer();
